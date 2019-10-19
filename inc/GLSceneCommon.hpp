acd962684fbf5b9bafd8c35bb160d850dd1995bc
jb <jean-baptiste.dupuy@epitech.eu>
2019-10-19 18:30:25 +0100

    start

---
+++ inc/GLSceneCommon.hpp
@@ -0,0 +1,13 @@
+#ifndef GLSCENE_COMMON
+#define GLSCENE_COMMON
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm.hpp>
+
+
+
+#endif //GLSCENE_COMMON
